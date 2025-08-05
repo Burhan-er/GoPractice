bfef04f15d64d3712d47a2707bc46380a2cc22a6
Burhan <jspro0950@gmail.com>
2025-08-05 19:50:54 +0300

    Update waitGroup.go

--- concurrency/synchronization/waitGroup.go
+++ concurrency/synchronization/waitGroup.go
@@ -2,32 +2,33 @@ package synchronization
 
 import (
 	"fmt"
-	"math/rand"
 	"sync"
 	"time"
 )
+
 func adWork(idx int) {
-	time.Sleep(time.Second * time.Duration(rand.Int()))
-	fmt.Printf("[GOROUTİNE %d]\n",idx)
+	time.Sleep(time.Second * 1)
+	fmt.Printf("[GOROUTİNE %d]\n", idx)
 }
-func WithWaitGroup(){
+func WithWaitGroup() {
 	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go func(idx int){
-			defer wg.Done() 
+		go func(idx int) {
+			defer wg.Done()
 			adWork(idx)
 		}(i)
 	}
 	wg.Wait()
-	
+
 	fmt.Printf("[WaitGroups] all goroutines finished.")
 }
-func WithOutWaitGroup(){
+func WithOutWaitGroup() {
 
 	for i := 0; i < 10; i++ {
 		go adWork(i)
 	}
+	time.Sleep(time.Second * 4)
 	fmt.Printf("[WitoutWaitGroups] finished.")
 
 }
